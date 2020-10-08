4c33e72e945f1e4575c61a2b8a652cf8a4ae61f8
Tanielian V. Barreira <tanielianvb@gmail.com>
2020-10-08 17:28:59 -0300

    Added main regions

--- cryptoedu-app/src/components/AppTopBar/AppTopBar.tsx
+++ cryptoedu-app/src/components/AppTopBar/AppTopBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Tooltip, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import ToggleButtons from "../ToggleButtons/ToggleButtons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
     },
     bar: {
       display: "flex",
@@ -32,22 +32,20 @@ function AppTopBar() {
   ];
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense" className={classes.bar} disableGutters>
-          <ToggleButtons initialSelectedOption="cs" options={languages} />
-          <Tooltip
-            title="Simulador de Algoritmos de Criptografia com Finalidade
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar variant="dense" className={classes.bar} disableGutters>
+        <ToggleButtons initialSelectedOption="cs" options={languages} />
+        <Tooltip
+          title="Simulador de Algoritmos de Criptografia com Finalidade
               Educacional"
-          >
-            <Typography variant="h6" color="secondary">
-              CryptoEdu
-            </Typography>
-          </Tooltip>
-          <ToggleButtons initialSelectedOption="des" options={types}/>
-        </Toolbar>
-      </AppBar>
-    </div>
+        >
+          <Typography variant="h6" color="secondary">
+            CryptoEdu
+          </Typography>
+        </Tooltip>
+        <ToggleButtons initialSelectedOption="des" options={types} />
+      </Toolbar>
+    </AppBar>
   );
 }
 
--- cryptoedu-app/src/pages/app/App.tsx
+++ cryptoedu-app/src/pages/app/App.tsx
@@ -1,10 +1,83 @@
 import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppTopBar from "../../components/AppTopBar/AppTopBar";
+import AppDrawer from "../../components/AppDrawer/AppDrawer";
+import { Toolbar, Typography } from "@material-ui/core";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appRoot: {
+      display: "flex",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="app">
+    <div className={classes.appRoot}>
       <AppTopBar />
+      <AppDrawer anchor="left">
+        <div className={classes.content}>
+          <Typography variant="h6" color="secondary">
+            C#
+          </Typography>
+          <Typography variant="body2" color="textPrimary">
+            Dados de execução do algorítimo.
+          </Typography>
+        </div>
+      </AppDrawer>
+      <main className={classes.content}>
+        <Toolbar variant="dense" />
+        <Typography variant="h6" color="secondary">
+          Code
+        </Typography>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+      <AppDrawer anchor="right">
+        <div className={classes.content}>
+          <Typography variant="h6" color="secondary">
+            DES
+          </Typography>
+          <Typography variant="body2" color="textPrimary">
+            Descrição e características do algorítimo.
+          </Typography>
+        </div>
+      </AppDrawer>
     </div>
   );
 }
