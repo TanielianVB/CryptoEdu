e12b1ce519fd34fdde1ef8d923cbace35a3f682e
Tanielian V. Barreira <tanielianvb@gmail.com>
2020-10-08 19:45:26 -0300

    Created side bar components

---
+++ cryptoedu-app/src/components/AlgorithmSideBar/AlgorithmSideBar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import AppDrawer from "../AppDrawer/AppDrawer";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
+
+function AlgorithmSideBar() {
+  const classes = useStyles();
+  return (
+    <AppDrawer anchor="right">
+      <div className={classes.content}>
+        <Typography variant="h6" color="secondary">
+          DES
+        </Typography>
+        <Typography variant="body2" color="textPrimary">
+          Descrição e características do algorítimo.
+        </Typography>
+        <Typography variant="body2" color="textPrimary">
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </div>
+    </AppDrawer>
+  );
+}
+
+export default AlgorithmSideBar;
--- cryptoedu-app/src/pages/app/App.tsx
+++ cryptoedu-app/src/pages/app/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppTopBar from "../../components/AppTopBar/AppTopBar";
-import AppDrawer from "../../components/AppDrawer/AppDrawer";
 import { Toolbar, Typography } from "@material-ui/core";
+import CodeSideBar from "../../components/CodeSideBar/CodeSideBar";
+import AlgorithmSideBar from "../../components/AlgorithmSideBar/AlgorithmSideBar";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,31 +44,7 @@ function App() {
         selectedAlgorithm={selectedAlgorithm}
         setSelectedAlgorithm={setSelectedAlgorithm}
       />
-      <AppDrawer anchor="left">
-        <div className={classes.content}>
-          <Typography variant="h6" color="secondary">
-            C#
-          </Typography>
-          <Typography variant="body2" color="textPrimary">
-            Dados de execução do algorítimo.
-          </Typography>
-          <Typography variant="body2" color="textPrimary">
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
-        </div>
-      </AppDrawer>
+      <CodeSideBar />
       <main className={classes.content}>
         <Toolbar variant="dense" />
         <Typography variant="h6" color="secondary">
@@ -103,31 +80,7 @@ function App() {
           posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
       </main>
-      <AppDrawer anchor="right">
-        <div className={classes.content}>
-          <Typography variant="h6" color="secondary">
-            DES
-          </Typography>
-          <Typography variant="body2" color="textPrimary">
-            Descrição e características do algorítimo.
-          </Typography>
-          <Typography variant="body2" color="textPrimary">
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
-        </div>
-      </AppDrawer>
+      <AlgorithmSideBar />
     </div>
   );
 }
