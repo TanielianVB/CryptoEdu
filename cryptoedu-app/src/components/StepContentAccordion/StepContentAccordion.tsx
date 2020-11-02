a76748d10e1f896d0d5199bcda8339a9c5975780
Tanielian V. Barreira <tanielianvb@gmail.com>
2020-11-02 13:42:07 -0300

    Enhanced StepContentAccordion

--- cryptoedu-app/src/components/StepContentAccordion/StepContentAccordion.tsx
+++ cryptoedu-app/src/components/StepContentAccordion/StepContentAccordion.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
 } from "@material-ui/core";
+import HelpOutlineRoundedIcon from "@material-ui/icons/HelpOutlineRounded";
 import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
 import StepContentTitle from "../../components/StepContentTitle/StepContentTitle";
 
@@ -14,10 +15,24 @@ interface StepContentAccordionProps {
 
 function StepContentAccordion(props: StepContentAccordionProps) {
   const { title, children } = props;
+
+  const [expanded, setExpanded] = useState(false);
+
+  const expandIcon = expanded ? (
+    <ExpandMoreRoundedIcon />
+  ) : (
+    <HelpOutlineRoundedIcon />
+  );
+
   return (
-    <Accordion square elevation={0}>
+    <Accordion
+      square
+      elevation={0}
+      expanded={expanded}
+      onChange={(event, isExpanded) => setExpanded(isExpanded)}
+    >
       <AccordionSummary
-        expandIcon={<ExpandMoreRoundedIcon />}
+        expandIcon={expandIcon}
         IconButtonProps={{ color: "secondary" }}
         style={{ paddingLeft: "0" }}
       >
