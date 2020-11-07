fb793d691c366454fa06568a1766d3bf967520db
Tanielian V. Barreira <tanielianvb@gmail.com>
2020-11-07 12:08:12 -0300

    Enhanced ExecutionExplanationText

--- cryptoedu-app/src/components/ExecutionExplanationText/ExecutionExplanationText.tsx
+++ cryptoedu-app/src/components/ExecutionExplanationText/ExecutionExplanationText.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 
 interface ExecutionExplanationTextProps {
   children: React.ReactNode;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    explanation: {
+      minHeight:
+        "calc(2 * " +
+        theme.typography.caption.lineHeight +
+        " * " +
+        theme.typography.caption.fontSize +
+        ")",
+    },
+  })
+);
+
 function ExecutionExplanationText(props: ExecutionExplanationTextProps) {
+  const classes = useStyles();
   return (
-    <Typography variant="caption" display="block" align="justify" gutterBottom>
+    <Typography
+      variant="caption"
+      display="block"
+      align="justify"
+      className={classes.explanation}
+      gutterBottom
+    >
       {props.children}
     </Typography>
   );
